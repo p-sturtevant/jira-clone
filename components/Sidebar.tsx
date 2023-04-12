846d3aa487549d0d2077ae00cc56b4facfa337b1
Parker Sturtevant <wsturtevant196@gmail.com>
2023-04-12 17:28:07 -0400

    Added sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import Card from './Card';
+//import Image from "next/image";
+import SidebarLink from './SidebarLink';
+
+const links = [
+	{ label: 'Home', icon: 'Grid', link: '/home' },
+	{
+		label: 'Calendar',
+		icon: 'Calendar',
+		link: '/calendar',
+	},
+	{ label: 'Profile', icon: 'User', link: '/profile' },
+	{
+		label: 'Settings',
+		icon: 'Settings',
+		link: '/settings',
+	},
+];
+
+const Sidebar = () => {
+	return (
+		<Card className="h-full w-40 flex items-center justify-between flex-wrap">
+			<div className="w-full flex justify-center items-center"></div>
+			{links.map((link, idx) => (
+				<SidebarLink key={idx} link={link} />
+			))}
+		</Card>
+	);
+};
+
+export default Sidebar;
