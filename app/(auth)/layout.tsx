c1017025fd11b54b3b977660177e90536b3451b9
Parker Sturtevant <wsturtevant196@gmail.com>
2023-04-20 16:27:20 -0400

    Layout changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,14 @@
 import '@/styles/global.css';
 import GlassPane from '@/components/GlassPane';
-// import { Inter } from '@next/font/google';
+import { Inter } from '@next/font/google';
 
-// const inter = Inter({
-// 	variable: '--font-inter',
-// });
+const inter = Inter({
+	variable: '--font-inter',
+});
 
 export default function AuthRootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.variable}>
 			<head />
 			<body className="h-screen w-screen rainbow-mesh p-6">
 				<GlassPane className="w-full h-full flex items-center justify-center">
